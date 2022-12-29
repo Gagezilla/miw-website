a5c4e3ab5aa99d2d3bf6a3ac177b44a957b5fe82
Gage Hundertmark <hundertmarkgage@gmail.com>
2022-12-29 13:48:48 -0700

    changed the logo a bit

--- src/routes/1/Footer.svelte
+++ src/routes/1/Footer.svelte
@@ -1,7 +1,7 @@
 <footer class="bg-clr2 font-poppins text-center py-10">
-    <span class="block mx-auto font-semibold text-[2rem] translate-y-[1.5rem]">Monarch</span>
+    <span class="block mx-auto font-semibold text-[1.85rem] translate-y-[1.5rem]">Monarch</span>
     <img class="mx-auto w-[10rem]" src="/pages/logo.png" alt="">
-    <span class="block mx-auto font-semibold text-[2rem] translate-y-[-1.5rem]">Works</span>
-    <div class="pt-5"></div>
+    <span class="block mx-auto font-semibold text-[1.5rem] translate-y-[-1.5rem]">Ironworks</span>
+    <div class="pt-6"></div>
     <small class="inline-block text-clr5 max-w-[16rem]"> Copyright - Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit</small>
 </footer>
