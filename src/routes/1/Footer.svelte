6ca03c150315db398b7535d9e06a6e05d678f90b
Gage Hundertmark <hundertmarkgage@gmail.com>
2022-12-29 13:40:21 -0700

    changed a padding thing

--- src/routes/1/Footer.svelte
+++ src/routes/1/Footer.svelte
@@ -2,6 +2,6 @@
     <span class="block mx-auto font-semibold text-[2rem] translate-y-[1.5rem]">Monarch</span>
     <img class="mx-auto w-[10rem]" src="/pages/logo.png" alt="">
     <span class="block mx-auto font-semibold text-[2rem] translate-y-[-1.5rem]">Works</span>
-    <div class="pt-8"></div>
+    <div class="pt-5"></div>
     <small class="inline-block text-clr5 max-w-[16rem]"> Copyright - Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit</small>
 </footer>
